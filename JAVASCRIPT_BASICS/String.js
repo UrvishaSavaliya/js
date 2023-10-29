d0b9cf02122debbf1c9881bbfa31179de93d9664
UrvishaSavaliya <82795619+UrvishaSavaliya@users.noreply.github.com>
2023-10-29 06:30:08 +0000

    String

---
+++ JAVASCRIPT_BASICS/String.js
@@ -0,0 +1,10 @@
+const name = "Urvisha"
+const repoCount = 50
+
+//console.log(name + repoCount  + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('Abcdefghi')
+console.log(gameName[0])
+console.log(gameName[1])
